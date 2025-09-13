56279b0b444899830774102ad0126ca0f73f9370
hadi2020490 <hadieditzz89@gmail.com>
2025-09-13 12:10:22 +0500

    Add files via upload

---
+++ js assignment/script.js
@@ -0,0 +1,47 @@
+alert("Hello world.");
+
+ let naam = "Ali";
+  alert(naam);
+
+  let age = 20;
+  alert(age);
+
+
+  let a = 10;
+  let b = 5;
+
+  
+  let sum = a + b;      
+  let difference = a - b; 
+  let product = a * b;    
+  let quotient = a / b;   
+
+  alert("Sum:" + sum);
+  alert("Difference:" + difference);
+  alert("Product:" + product);
+  alert("Quotient:" + quotient);
+
+
+  let x = 10;
+  let y = 3;
+
+
+  let remainder = x % y;     
+  let power = x ** y;        
+  x++;                       
+  y--;                       
+
+  alert("Remainder (10 % 3): " + remainder);
+  alert("Power (10 ** 3): " + power);
+  alert("After x++: " + x);
+  alert("After y--: " + y);
+
+
+  let result1 = 10 + 5 * 2;      
+    let result2 = (10 + 5) * 2;    
+
+  alert("Without parentheses: " + result1); 
+  alert("With parentheses: " + result2);    
+
+let userName = prompt("What is your name?");
+  alert("Hello, " + userName + "!");
